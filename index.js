c3a430a225089d7a9302498647fe0d256731507f
Tilman Raendchen <tilman@medallyon.me>
2021-08-12 00:45:47 +0100

    Create version 1.0.0

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core")
+	, github = require("@actions/github")
+	, fs = require("fs-extra");
 
 // most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+async function run()
+{
+	try
+	{
+		const payload = github.context.payload
+			, tag = payload.release.tag_name;
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+		const pkg = await fs.readJson("./package.json")
+			, originalVersion = pkg.version;
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+		pkg.version = tag.replace("v", "");
+		await fs.outputJson("./package.json", pkg, {
+			spaces: 2
+		});
+
+		core.info(`Modified version number in package.json from ${originalVersion.replace("v", "")} to ${pkg.version}`);
+	}
+
+	catch (error)
+	{
+		core.setFailed(error.message);
+	}
 }
 
 run();
