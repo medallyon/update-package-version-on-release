304786874a3d8b37eaf142606653d5e37e54b712
Tilman Raendchen <tilman@medallyon.me>
2021-08-12 01:29:37 +0100

    Adapt semver regex & throw errors accordingly

--- dist/index.js
+++ dist/index.js
@@ -529,16 +529,21 @@ async function run()
 {
 	try
 	{
-		const payload = github.context.payload
-			, tag = payload.release.tag_name;
+		const payload = github.context.payload;
+		let tag = payload.release.tag_name;
 
-		if (!/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm.test(tag))
-			core.setFailed("Release Tag does not match semantic versioning.");
+		if (tag.startsWith("v"))
+			tag = tag.replace("v", "");
+
+		if (!/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm.test(tag))
+		{
+			throw new Error("Release Tag does not match semantic versioning.");
+		}
 
 		const pkg = await fs.readJson("./package.json")
 			, originalVersion = pkg.version;
 
-		pkg.version = tag.replace("v", "");
+		pkg.version = tag;
 		await fs.outputJson("./package.json", pkg, {
 			spaces: 2
 		});
